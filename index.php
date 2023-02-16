4f7409f2088b01dd8c26bc95132113465cea0fc1
Your Name <you@example.com>
2023-02-16 06:44:04 +0330

    h

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="fa">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset:utf-8" />
+<meta http-equiv="Content-language" content="fa" />
+<meta name="description" content="###" />
+<meta name="keywords" content="###" />
+<title></title>
+<link rel="stylesheet" type="text/css" href="css/style.css" >
+<link rel="stylesheet" type="text/css" href="icofont/icofont.css" >
+<link rel="stylesheet" type="text/css" href="icofont/icofont.min.css" >
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
+</head>
+<style type="text/css">
+@font-face {
+	font-family: yekan;
+	font-style: normal;
+	font-weight:;
+	src:url('./fonts/yekan.eot');
+	src:url('./fonts/yekan.eot?#iefix') format('embedded-opentype'),
+	url('./fonts/yekan.woff2') format('woff2'),
+	url('./fonts/yekan.woff') format('woff'),
+	url('./fonts/yekan.ttf') format('truetype');
+}
+</style>
+<body class="body" >
+ <nav class="nav-bar" >
+   <ul><center>
+     <li><a href="#" ></a></li>
+     <a href="#" ><li class="mci" >ایران</li></a>
+     <a href="#" ><li class="mtn" >هم</li></a>
+    <a href="gg" ><button href="aaa" class="btn btn-danger" method="dddd"  >ggy</button></a>
+   </center></ul>
+ </nav>
+   <form class="header" align="center" >
+    <center>
+    <table>
+     <tr>
+      <td class="iran" >فـیـ</td>
+      <td class="icofont-toy-elephant icofont-3x" ></td>
+      <td class="iran" >ـل</td>
+      <td class="iran" >تــر</td>
+      </tr>
+      </table>
+    <!--
+     <span class="icofont-toy-elephant icofont-3x" style="font-family:yekan;">ت</span>
+     -->
+    </center>
+   </form>
+   <form class="form1" >
+     <span style="marigin-right:3px">برنامه و کانفیگ </span>
+   </form>
+   <form class="form2" >
+   <span>سایت دردست تعمیر است.پس از تغییرات ناگهانی شوکه نشوید</span>
+  
+   <table align="center" >
+   <tr >
+   <th style="width:15%" class="tth" >شماره</th>
+   <th style="width:45%" class="tth"  >نام فایل</th>
+   <th style="width:30%" class="tth" >تاریخ</th>
+   <th style="width:20%" class="tth" >دانلود</th>
+   </tr>
+   <tr>
+   <td>11</td>
+   <td>11.bak</td>
+   <td>1401/11/27</td>
+   <td><a href="https://mayammad2.github.io/myp2/files/11.bak" class="btn btn-danger" >دانلودg</a></td>
+   </tr>
+   </table>
+   
+   </form>
+   <form class="form2" ><div>
+
+
+
+
+
+
+   </form>
+</body>
+</html>
